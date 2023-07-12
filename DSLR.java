e932c908351afc8702811637d065980f750adeb3
RachithaNC <124651675+RachithaNC@users.noreply.github.com>
2023-07-12 14:38:46 +0530

    Add files via upload

---
+++ DSLR.java
@@ -0,0 +1,30 @@
+class  DSLR{
+
+
+	/*static String connectivityTechnology;
+	static int price;
+	static String falshMemory;
+	static String specialFeature;
+	static String screenSize;
+	static String brand;*/
+	
+	
+	public static void main(String Camera[]){
+	
+ String connectivityTechnology = "wi-fi";
+ int price = 2199;
+ String falshMemory = "Micro SD";
+ String specialFeature = "waterproof";
+ String screenSize = "2inches";
+ String brand = "DSLR";
+	
+	
+	System.out.println("The Camera connectivityTechnology logyis "+connectivityTechnology);
+	System.out.println("The Camera brand is "+brand);
+	System.out.println("The Camera price is "+price);
+	System.out.println("The Camera specialFeature is "+specialFeature );
+	System.out.println("The Camera screenSize is "+screenSize);
+	
+	}
+	}
+	
